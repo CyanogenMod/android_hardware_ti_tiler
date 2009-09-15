0391f906af0b2c09477c91b1c46ba1837d870c0d
Lajos Molnar <molnar@ti.com>
2009-09-15 06:46:56 -0500

    Fixed libprocmgr pc package info file

--- mk-libprocmgr-pc.sh
+++ mk-libprocmgr-pc.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 # 
-# A simple script to generate a libdspbridge.pc with the proper paths..  This
-# is only a temporary solution until libdspbridge is autotools-ified
+# A simple script to generate a libprocmgr.pc with the proper paths..  This
+# is only a temporary solution until libprocmgr is autotools-ified
 # 
 # Usage:
 # 
-#    mk-libdspbridge-pc.sh /path/to/staging/usr
+#    mk-libprocmgr-pc.sh /path/to/staging/usr
 # 
 # the parameter should specify the ${prefix} dir, ie. where the staging area
 # is..  it should include the "/usr" if everything will be installed in
-# /usr/lib, /usr/include, etc relative to the staging area
 # 
 
 PREFIX=$1
@@ -25,5 +24,5 @@ Description: ProcMgr Library
 Version: 2.0.0
 Requires: 
 Libs: -L\${libdir} -lprocmgr
-Cflags: -I\${includedir}\syslink
+Cflags: -I\${includedir}/syslink
 EOF
