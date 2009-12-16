184e24c360f8739318c4c135d1e66b53748c8a57
Lajos Molnar <lajos@duna.(none)>
2009-12-16 16:11:26 -0600

    Updated build directions now that syslink uses autotools.

--- build_directions.sh
+++ build_directions.sh
@@ -1,71 +1,59 @@
 #!/bin/sh
 
+# *****************************************************************************
+#  THIS DOCUMENT DESCRIBES THE BUILD STEPS NEEDED TO BUILD THE TWO COMPONENTS
+#  OF THIS GIT TREE: MEMMGR AND D2C
+#
+#  The document is formatted as a shell script and is organized into 4
+#  sections.  The first section sets up the environment variables needed
+#  for the build sections.  You will need to set up the same variables if
+#  you plan to build the components.
+#
+#  Then you need to build memmgr, syslink and d2c components in that order
+#  as they depend on one another.
+#
+#  You may also choose to execute this document and manually enter the
+#  required environment variables
+#
+# *****************************************************************************
+
 DIR=`dirname $0`
 HOST=arm-none-linux-gnueabi
 
 # 0) Gather requirements
 # =============================================================================
 
-# path to target filesystem
+# PREFIX: path to target filesystem
 echo "Enter PREFIX (currently '$PREFIX'):\c"
 read VALUE
 export PREFIX=${VALUE:=$PREFIX}
 
-# path to userspace-syslink git root
+# USERSPACE_SYSLINK: path to userspace-syslink git root
 echo "Enter path to userspace-syslink git root (currently '$USERSPACE_SYSLINK'):\c"
 read VALUE
 export USERSPACE_SYSLINK=${VALUE:=$USERSPACE_SYSLINK}
 
-# path to tiler-userspace git root
-export TILER_USERSPACE=`readlink -f $DIR`
-
-# 4) Need path to toolchain
+# PATH: should contain the build toolchain
 
 #.. first find gcc
 TOOL=`which ${HOST}-gcc`
-if [ ! -e $TOOL ]
+if [ "$TOOL" ]
 then
-    echo "Could not find gcc"
-    exit
+    TOOLBIN=`dirname $TOOL`
+else
+    echo "Could not find ${HOST}-gcc in PATH."
+    TOOLBIN=.
 fi
 
-TOOLBIN=`dirname $TOOL`
 echo "Enter tool path (currently '$TOOLBIN'):\c"
 read VALUE
 TOOLBIN=${VALUE:=$TOOLBIN}
 
 echo TOOLBIN           is ${TOOLBIN}
 echo PREFIX            is ${PREFIX}
-echo TILER_USERSPACE   is ${TILER_USERSPACE}
 echo USERSPACE_SYSLINK is ${USERSPACE_SYSLINK}
 
 export PATH=${TOOLBIN}:$PATH
-
-#.. find libgcc
-TOOL=`which ${HOST}-gcc`
-TOOLDIR=`dirname $TOOL`
-LIBGCC=$TOOLDIR/../lib/gcc/$HOST/*/libgcc.a
-if [ ! -e $LIBGCC ]
-then
-    echo "Could not find libgcc.a"
-    exit
-fi
-echo Found libgcc.a in $LIBGCC
-LIBRT=$TOOLDIR/../$HOST/libc/lib/librt*.so
-if [ ! -e $LIBRT ]
-then
-    echo "Could not find librt.so"
-    exit
-fi
-echo Found librt.so in $LIBRT
-LIBPTHREAD=$TOOLDIR/../$HOST/libc/lib/libpthread*.so
-if [ ! -e $LIBPTHREAD ]
-then
-    echo "Could not find libpthread.so"
-    exit
-fi
-echo Found libpthread.so in $LIBPTHREAD
-
 
 # 1) First you need to build memmgr
 # =============================================================================
@@ -75,7 +63,7 @@ ENABLE_UNIT_TESTS=--enable-unit-tests
 
 #.. uncomment to export the tilermgr.h header - this is currently needed by
 #   syslink
-ENABLE_TILERMGR=--enable-tilermgr
+# ENABLE_TILERMGR=--enable-tilermgr
 
 cd ${TILER_USERSPACE}/memmgr
 ./bootstrap.sh
@@ -87,27 +75,10 @@ make install
 # 2) Second you need to build syslink
 # =============================================================================
 
-#.. need libgcc.a, librt.so and libpthread.so
-mkdir -p ${PREFIX}/lib
-cp $LIBGCC ${PREFIX}/lib
-cp `dirname $LIBRT`/librt*.so* ${PREFIX}/lib
-cp `dirname $LIBPTHREAD`/libpthread*.so* ${PREFIX}/lib
-
-#.. syslink prefix needs a target subdirectory, so we will create link to the
-#   parent
-if [ ! -d ${PREFIX}/target ]; then ln -s ${PREFIX} ${PREFIX}/target; fi
-
 cd ${USERSPACE_SYSLINK}/syslink
-./autogen.sh
-./configure --prefix ${PREFIX} --bindir ${PREFIX}/syslink --host ${HOST} ${ENABLE_DEBUG}
-cd -
-cd ${USERSPACE_SYSLINK}/syslink/api
-make clean
-make
-make install
-cd -
-cd ${USERSPACE_SYSLINK}/syslink/daemons
-export TILER_INC_PATH=${TILER_USERSPACE}/memmgr
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+./bootstrap.sh
+./configure --prefix ${PREFIX} --host ${HOST} ${ENABLE_DEBUG}
 make clean
 make
 make install
@@ -118,11 +89,9 @@ cd -
 
 cd ${TILER_USERSPACE}/d2c
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
-./mk-libprocmgr-pc.sh ${PREFIX} ${USERSPACE_SYSLINK}
 ./bootstrap.sh
 ./configure --prefix ${PREFIX} --host ${HOST} ${ENABLE_UNIT_TESTS}
 make clean
 make
 make install
-
 cd -
