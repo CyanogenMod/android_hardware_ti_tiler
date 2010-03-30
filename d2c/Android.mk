4068f7e22385b11cf688ac47e58ac072c08d07d9
Keith Deacon <kdeacon@ti.com>
2010-03-30 10:40:40 -0500

    Android build changes

--- d2c/Android.mk
+++ d2c/Android.mk
@@ -12,9 +12,10 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../utils \
 	$(LOCAL_PATH)/../memmgr \
-	$(LOCAL_PATH)/../../syslink/api/include \	
+	hardware/ti/syslink/syslink/api/include \	
+#
 	
-LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
+#LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
 
 #-DOMAP_3430
 
--- memmgr/Android.mk
+++ memmgr/Android.mk
@@ -14,7 +14,7 @@ LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../utils \
 
 	
-LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
+#LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
 
 #-DOMAP_3430
 
