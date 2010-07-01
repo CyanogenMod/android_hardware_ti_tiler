c8de546ae110fecac285928e73ba127ea5c60689
Lajos Molnar <molnar@ti.com>
2010-07-01 10:02:08 -0500

    MEMMGR: Separated debug flags from one another.
    Previously, if __DEBUG__ was not defined, none of the other
    debug messages were printed, even if __DEBUG_ENTRY__ or
    __DEBUG_ASSERT__ were defined.
    
    Now the 3 flags operate independently.
    
    Commit-Id: I816788788d8769ff5e22178863587ad2a4b9f027
    Signed-off-by: Lajos Molnar <molnar@ti.com>
    Signed-off-by: David Sin <davidsin@ti.com>

--- debug_utils.h
+++ debug_utils.h
@@ -17,6 +17,8 @@
 #ifndef _DEBUG_UTILS_H_
 #define _DEBUG_UTILS_H_
 
+#include <stdio.h>
+
 /*#define __DEBUG__*/
 /*#define __DEBUG_ENTRY__*/
 /*#define __DEBUG_ASSERT__*/
@@ -31,14 +33,18 @@
  *    ==> val is 5 at test.c:56:main()
  */
 /* debug print (fmt must be a literal); adds new-line. */
-#ifdef __DEBUG__
-#define P(fmt, ...) S_ { fprintf(stdout, fmt "\n", ##__VA_ARGS__); fflush(stdout); } _S
-#else
-#define P(fmt, ...)
-#endif
+#define __DEBUG_PRINT(fmt, ...) S_ { fprintf(stdout, fmt "\n", ##__VA_ARGS__); fflush(stdout); } _S
 
 /* debug print with context information (fmt must be a literal) */
-#define DP(fmt, ...) P(fmt " at %s(" __FILE__ ":%d)", ##__VA_ARGS__, __FUNCTION__, __LINE__)
+#define __DEBUG_DPRINT(fmt, ...) __DEBUG_PRINT(fmt " at %s(" __FILE__ ":%d)", ##__VA_ARGS__, __FUNCTION__, __LINE__)
+
+#ifdef __DEBUG__
+#define P(fmt, ...) __DEBUG_PRINT(fmt, ##__VA_ARGS__)
+#define DP(fmt, ...) __DEBUG_DPRINT(fmt, ##__VA_ARGS__)
+#else
+#define P(fmt, ...)
+#define DP(fmt, ...)
+#endif
 
 /* ---------- Program Flow Debug Macros ---------- */
 
@@ -72,13 +78,13 @@
 
 #ifdef __DEBUG_ENTRY__
 /* function entry */
-#define IN P("in %s(" __FILE__ ":%d)", __FUNCTION__, __LINE__)
+#define IN __DEBUG_PRINT("in %s(" __FILE__ ":%d)", __FUNCTION__, __LINE__)
 /* function exit */
-#define OUT P("out %s(" __FILE__ ":%d)", __FUNCTION__, __LINE__)
+#define OUT __DEBUG_PRINT("out %s(" __FILE__ ":%d)", __FUNCTION__, __LINE__)
 /* function abort (return;)  Use as { RET; return; } */
-#define RET DP("out() ")
+#define RET __DEBUG_DPRINT("out() ")
 /* generic function return */
-#define R(val,type,fmt) E_ { type __val__ = (type) val; DP("out(" fmt ")", __val__); __val__; } _E
+#define R(val,type,fmt) E_ { type __val__ = (type) val; __DEBUG_DPRINT("out(" fmt ")", __val__); __val__; } _E
 #else
 #define IN
 #define OUT
@@ -112,12 +118,12 @@
 #ifdef __DEBUG_ASSERT__
 #define A(exp,cmp,val,type,fmt) E_ { \
     type __exp__ = (type) (exp); type __val__ = (type) (val); \
-    if (!(__exp__ cmp __val__)) DP("assert: %s (=" fmt ") !" #cmp " " fmt, #exp, __exp__, __val__); \
+    if (!(__exp__ cmp __val__)) __DEBUG_DPRINT("assert: %s (=" fmt ") !" #cmp " " fmt, #exp, __exp__, __val__); \
     __exp__; \
 } _E
 #define CHK(exp,cmp,val,type,fmt) S_ { \
     type __exp__ = (type) (exp); type __val__ = (type) (val); \
-    if (!(__exp__ cmp __val__)) DP("assert: %s (=" fmt ") !" #cmp " " fmt, #exp, __exp__, __val__); \
+    if (!(__exp__ cmp __val__)) __DEBUG_DPRINT("assert: %s (=" fmt ") !" #cmp " " fmt, #exp, __exp__, __val__); \
 } _S
 #else
 #define A(exp,cmp,val,type,fmt) (exp)
@@ -136,7 +142,7 @@
 #ifdef __DEBUG_ASSERT__
 #define NOT(exp,cmp,val,type,fmt) E_ { \
     type __exp__ = (type) (exp); type __val__ = (type) (val); \
-    if (!(__exp__ cmp __val__)) DP("assert: %s (=" fmt ") !" #cmp " " fmt, #exp, __exp__, __val__); \
+    if (!(__exp__ cmp __val__)) __DEBUG_DPRINT("assert: %s (=" fmt ") !" #cmp " " fmt, #exp, __exp__, __val__); \
     !(__exp__ cmp __val__); \
 } _E
 #else
