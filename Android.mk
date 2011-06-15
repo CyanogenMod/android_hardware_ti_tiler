f347ed39487832f5c039c7005dbc664535a64018
Iliyan Malchev <malchev@google.com>
2011-06-15 13:36:00 -0700

    MEMMGR: Clean up Android Makefiles
    Cleaned up the makefiles by removing the local
    include paths and compiler options.
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- Android.mk
+++ Android.mk
@@ -4,66 +4,32 @@ ifeq ($(TARGET_BOARD_PLATFORM),omap4)
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
-
-LOCAL_SRC_FILES := \
-                memmgr.c \
-                tilermgr.c \
-
-
-LOCAL_C_INCLUDES += \
-        $(LOCAL_PATH)/ \
-
-LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
-
-LOCAL_MODULE    := libtimemmgr
-LOCAL_MODULE_TAGS:= optional tests
-
+LOCAL_SRC_FILES := memmgr.c tilermgr.c
+LOCAL_MODULE := libtimemmgr
+LOCAL_MODULE_TAGS := optional tests
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_SRC_FILES := memmgr_test.c testlib.c
-LOCAL_C_INCLUDES += \
-        $(LOCAL_PATH)/ \
-
-LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
-
 LOCAL_SHARED_LIBRARIES := libtimemmgr
-
 LOCAL_MODULE := memmgr_test
-LOCAL_MODULE_TAGS:= optional tests
+LOCAL_MODULE_TAGS := optional tests
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_SRC_FILES := utils_test.c testlib.c
-LOCAL_C_INCLUDES += \
-        $(LOCAL_PATH)/ \
-
-LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
-
 LOCAL_SHARED_LIBRARIES := libtimemmgr
-
-LOCAL_MODULE    := utils_test
-LOCAL_MODULE_TAGS:= optional tests
+LOCAL_MODULE := utils_test
+LOCAL_MODULE_TAGS := optional tests
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_SRC_FILES := tiler_ptest.c
-LOCAL_C_INCLUDES += \
-        $(LOCAL_PATH)/ \
-
-LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -fpic
-
 LOCAL_SHARED_LIBRARIES := libtimemmgr
-
 LOCAL_MODULE    := tiler_ptest
 LOCAL_MODULE_TAGS:= optional tests
 include $(BUILD_EXECUTABLE)
